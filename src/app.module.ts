5666ca42856e086295e8090d5745ad9b96e18a5f
Robnelito <christianorak@gmail.com>
2023-12-09 14:00:56 +0300

    add login features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
